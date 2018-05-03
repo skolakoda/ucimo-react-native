c63c684a2b725a9c5a913c4dfe878dfe97415563
Damjan Pavlica <mudroljub@gmail.com>
2018-05-03 14:28:02 +0200

    Create View.js

---
+++ View.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { AppRegistry, View, StyleSheet } from 'react-native'
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.box} />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  box: {
+    width: 200,
+    height: 200,
+    backgroundColor: 'skyblue',
+    borderWidth: 20,
+    borderColor: 'steelblue',
+  },
+})
+
+AppRegistry.registerComponent('App', App)
